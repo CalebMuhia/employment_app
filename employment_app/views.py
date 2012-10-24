5364d695c782e991920600e82dfdca55c2c20a27
Paul Backhouse <paulb@aptivate.org>
2012-10-24 16:01:00 +0100

    "Home" template makes more sense at root of template directory.

--- employment_app/views.py
+++ employment_app/views.py
@@ -7,5 +7,5 @@ from django.template import RequestContext
 
 def home(request):
     """ shows the homepage """
-    return render_to_response("employment_app/home.html",
+    return render_to_response("home.html",
                               context_instance=RequestContext(request))

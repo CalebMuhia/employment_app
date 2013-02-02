b1bb12c03144766a3e363045ecac14dbe1dee105
¨calebmuhia¨ <clbnjoroge@gmail.com>
2013-02-02 21:46:32 +0300

    adding a new template

--- employment_program/settings.py
+++ employment_program/settings.py
@@ -26,14 +26,27 @@ ADMINS = (
 MANAGERS = ADMINS
 
 #DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'emp_app',                      # Or path to database file if using sqlite3.
+#        'USER': 'root',                      # Not used with sqlite3.
+#        'PASSWORD': 'phoneboth',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
+
+
+
+#DATABASES = {
  #   'default': {
  #       'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
  #       'NAME': os.path.join(PROJECT_ROOT,'employment.db'), # Or path to database file if using sqlite3.
  #   }
 #}
 
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+#import dj_database_url
+#DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
